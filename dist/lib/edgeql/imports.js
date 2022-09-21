1f79c909d63157c86ad7244702fcde15504a5de9
jdheeter <johndheeter@gmail.com>
2022-09-21 00:05:28 -0500

    rebuild

--- dist/lib/edgeql/imports.js
+++
@@ -1,5 +0,0 @@
-export * as edgedb from "edgedb";
-export { spec } from "./__spec__.js";
-export * as syntax from "./syntax/syntax.js";
-export * as castMaps from "./castMaps.js";
-//# sourceMappingURL=imports.js.map
