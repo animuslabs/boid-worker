d5e2508a593826aa038a1921702f9c670988ba43
jdheeter <johndheeter@gmail.com>
2023-01-06 19:10:28 -0600

    debugging

---
+++ src/util/dbActionsOnBlockTime.ts
@@ -0,0 +1,28 @@
+import fs from "fs-extra"
+import db, { Prisma } from "lib/db"
+import { actionMap } from "lib/injest"
+import { parseISOString } from "lib/utils"
+
+
+async function init(timeStamp:string) {
+  if (!timeStamp || typeof timeStamp != "string") throw (new Error("must provide valid ISO timestamp"))
+  const blockDate = parseISOString(timeStamp)
+  let results:any[] = []
+  for (let act of Object.keys(actionMap)) {
+    let dbTable = act as Prisma.ModelName
+    const query = { where: { timeStamp: { equals: timeStamp } } }
+    console.log(query)
+
+    const response = await db[dbTable].findMany(query)
+    console.log(response)
+
+    if (response) response.forEach(el => results.push(el))
+  }
+  const writeFile = "../actions-" + blockDate.toISOString()
+  fs.writeJsonSync(writeFile, results, { spaces: 2 })
+  console.log(results)
+  console.log("wrote to file:", writeFile)
+}
+
+const params = process.argv.slice(2)
+init(params[0]).catch(console.error)
