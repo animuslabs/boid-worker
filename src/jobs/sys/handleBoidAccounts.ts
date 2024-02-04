c65a458c86b27d7779c0285b6d1073a69655881e
jdheeter <johndheeter@gmail.com>
2024-02-04 00:07:39 +0000

    cleanup function

--- src/jobs/sys/handleBoidAccounts.ts
+++ src/jobs/sys/handleBoidAccounts.ts
@@ -27,12 +27,13 @@ import { sysActions } from "lib/actions"
 
 // claim if account needs to be claimed
 async function claimAccount(account:Account, config:Config, round:number) {
-  log.info("Claiming:", account.boid_id.toString(), round, account.power.last_claimed_round.toNumber())
   if (account.boid_id.toString() == "boid") return
   const elapsed = round - account.power.last_claimed_round.toNumber()
-  if (elapsed === 0) return
+  if (elapsed < 1) return
   const hasModPwr = account.power.boosters.find(el => el.pwr_add_per_round.toNumber() > 0 && el.aggregate_pwr_remaining.toNumber() > 0 && el.expires_round.toNumber() > round)
   if (account.power.rating.toNumber() == 0 && !hasModPwr) return
+  log.debug("elapsed:", elapsed)
+  log.info("Claiming:", account.boid_id.toString(), round, account.power.last_claimed_round.toNumber())
   await doAction("power.claim", { boid_id: account.boid_id }, env.contracts.system)
 }
 
@@ -41,11 +42,13 @@ async function init() {
     const allAccounts = await getFullTable({ tableName: "accounts", contract: env.contracts.system }, Account)
     log.info("Got all Boid accounts:", allAccounts.length)
     const round = await currentRound()
+    log.info("Current round:", round.toFixed())
     const config = await getSysConf()
     for (const account of allAccounts) {
       await claimAccount(account, config, round)
       // await handleExpired(account, new ActionPusher(), round)
     }
+    log.info("Job finished successfuly")
     // let claimed = 0
     // let pwrModsCleared = 0
     // const pusher = new ActionPusher()
